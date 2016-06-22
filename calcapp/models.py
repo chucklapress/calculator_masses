a48f06fbbbe669fd437a7a5a0c2b7f2289215062
chucklapress <charleslapress@gmail.com>
2016-06-22 15:58:29 -0400

    modified template, migrated model

--- calcapp/models.py
+++ calcapp/models.py
@@ -7,7 +7,6 @@ class SavedCalc(models.Model):
     operators = models.CharField(max_length=15)
     right = models.IntegerField()
     total = models.FloatField()
-    loguser = models.CharField(max_length=25)
+    loguser = models.ForeignKey('auth.User')
 
-class SaveUser(models.Model):
-    saveuser = models.ForeignKey(SavedCalc)
+
